9ecaf0644d81fae7bacbe0c6fdd990e362a451d7
jeremias-oss <62854991+jeremias-oss@users.noreply.github.com>
2024-08-19 12:38:20 -0300

    vuelta atras

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ window.addEventListener('load', function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: '.dots_m',
-        draggable: true,
-        scrollLock: true,
-
+        rewind: true,
+        //draggable: true,
+        arrows: {
+            prev: '.button_left_m',
+            next: '.button_right_m'
+        },
     });
     new Glider(document.querySelector('.glider_lista_t'), {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: '.dots_t',
-        draggable: true,
+        rewind: true,
+        //draggable: true,
         arrows: {
             prev: '.button_left_t',
             next: '.button_right_t'
@@ -21,7 +25,8 @@ window.addEventListener('load', function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: '.dots',
-        draggable: true,        
+        rewind: true,
+        //draggable: true,        
         arrows: {
             prev: '.button_left',
             next: '.button_right'
